53856afd9138dbd1c9013964c866faeea9d107ce
p schuhmacher <contact@pschuhmacher.com>
2024-11-04 11:44:02 +0100

    publie demo live

---
+++ demos/environement-dev/README.md
@@ -0,0 +1,9 @@
+## Lancer le projet
+
+~~~bash
+npm run dev
+~~~
+
+## Arrêter le projet
+
+Ctr+c dans le terminal où le serveur web tourne.
---
+++ demos/environement-dev/index.js
@@ -0,0 +1,58 @@
+let str = "Une chaine de caractères";
+
+let i = 10;
+
+let numbers = [1, 2, 3, 5, 7];
+
+//Déclarer un objet JavaScript
+//Un dictionnaire de clefs/valeur
+let obj = {
+    lastName : 'Doe',
+    firstName : 'Jane'
+}
+
+//Acceder à une propriété d'un objet
+console.log(obj.firstName, obj.lastName);
+
+
+//Constructeur d'objets qui représentent une personne
+//Nom, prénom
+
+let jane = {
+    firstName: 'Jane',
+    lastName: 'Doe',
+}
+
+let john = {
+    firstName: 'John',
+    lastName: 'Doe'
+}
+
+//Fabrique des objets à la chaine avec les mêmes propriétés
+//La fonction constructeur prend en arguments les données
+//pour fabriquer et initialiser l'objet
+function Person(firstName, lastName){
+    this.firstName = firstName;
+    this.lastName = lastName;
+
+    //Enrichir notre modèle de "Personne"
+    //avec une fonction. Une fonction appartenant
+    //à un objet => une méthode
+    this.greet = function(){
+        //Dire bonjour sur la console
+        console.log("Hi, I'm " + this.firstName + " " + this.lastName)
+    }
+}
+//Créer un objet à partir de la fonction constructeur
+let lilou = new Person('Lilou', 'Doe');
+
+//Tests
+
+console.log(jane, john, lilou);
+
+//Appel de la fonction propre à l'objet (méthode)
+jane.greet();
+//Est ce que je peux faire ça ?
+john.greet();
+
+
